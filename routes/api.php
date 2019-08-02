bcf0cecd02d9300db3fa409dcb21d3d50589b354
merbravo <mercybravobsc@hotmail.com>
2019-08-02 16:27:05 -0500

    Creaci'on de las respectivas rutas APIs

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/v1/marca',('MarcaController'));
+Route::resource('/v2/auto',('AutoController'));
